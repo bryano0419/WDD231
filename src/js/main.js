89bec5757296de9fd435fa5c040012005ae4a844
Bryan Osorio <you@example.com>
2025-09-23 03:30:35 -0600

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+document.title = parkData.fullName;
+const heroImage = document.querySelector('.hero-banner img');
+heroImage.src = parkData.images[0].url;
+heroImage.alt = parkData.images[0].altText;
+const heroInfoSection = document.querySelector('.hero-banner__text');
+heroInfoSection.innerHTML = parkInfoTemplate({
+    name: parkData.fullName,
+    designation: parkData.designation,
+    states: parkData.states
+});
